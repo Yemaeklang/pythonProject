9dcbb71a604e7eb8c79ebe5fe0dba0b334cc60a7
Jaipil Lee <yemaeklang@gmail.com>
2023-03-04 10:40:14 +0900

    commit

--- Codeit_Lecture/00_practice/binary_search.py
+++ Codeit_Lecture/00_practice/binary_search.py
@@ -1,16 +1,15 @@
 def binary_search(element, some_list):
-    if element not in some_list:
-        return None
-    else:
-        temp_min = 0
-        temp_max = len(some_list)
-        while True:
-            if element == some_list[(temp_min + temp_max) // 2]:
-                return (temp_min + temp_max) // 2
-            elif element > some_list[(temp_min + temp_max) // 2]:
-                temp_min = (temp_min + temp_max) // 2 + 1
-            else:
-                temp_max = (temp_min + temp_max) // 2
+    index_min = 0
+    index_max = len(some_list) - 1
+    while index_min <= index_max:
+        mid_index = (index_min + index_max) // 2
+        if element == some_list[mid_index]:
+            return mid_index
+        elif element > some_list[mid_index]:
+            index_min = mid_index + 1
+        else:
+            index_max = mid_index - 1
+    return None
 
 
 print(binary_search(2, [2, 3, 5, 7, 11]))
