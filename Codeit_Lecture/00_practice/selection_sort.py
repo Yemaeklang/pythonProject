e3352b74e74a277d3f3fdce2c5f03b733d0ee5db
Yemaeklang <yeamaeklang@gmail.com>
2023-03-07 02:03:28 +0900

    init

---
+++ Codeit_Lecture/00_practice/selection_sort.py
@@ -0,0 +1,17 @@
+def sorting(list):
+    i = 0
+    while i < len(list) - 1:
+        j = i + 1
+        while j < len(list):
+            if list[i] > list[j]:
+                temp = list[i]
+                list[i] = list[j]
+                list[j] = temp
+            j += 1
+        i += 1
+    return list
+
+
+my_numbers = [4, 2, 7, 1, 9, 3, 8, 5, 6]
+
+print(sorting(my_numbers))
